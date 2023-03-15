1e286e7a14ac678361b9ef4741e0eb59dc4fd90b
Raita Kondo <raycondo915@gmail.com>
2023-03-15 16:26:45 +0700

    commit again

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,7 @@ const Header = ({ siteTitle }) => (
   <header>
     <div className="header1">
       <h1>Welcome to Dog For You!</h1>
+      <h2>I wish you could find your dream dog!</h2>
     </div>
   </header>
 )
